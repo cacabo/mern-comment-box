34cc97721ac12eab57145e1ce15b3140ba4256dd
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-06 23:51:25 -0700

    in too deep--hope to return to this later

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Comment = require('./model/comments');
+var dbCredentials = require('./dbCredentials.json');
 
 //and create our instances
 var app = express();
@@ -12,6 +14,13 @@ var router = express.Router();
 
 //set our port to either a predetermined port number if you have set it up, or 3001
 var port = process.env.API_PORT || 3001;
+
+//db configure
+mongoose.connect(`mongodb://${dbCredentials.username}:${dbCredentials.password}>@ds151062.mlab.com:51062/cameron-cabo-mern-comment-box`, (err, db) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
 
 //now we should configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
